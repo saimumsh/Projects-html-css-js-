31486efd4301d98485000749c1d1f7bb2439da66
Shahadat Hossain <sleepyheadprofessor@gmail.com>
2023-11-24 00:05:00 +0600

    Hidden Search Widget

---
+++ Hidden Search Widget/home.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.container');
+
+btn.addEventListener('click',()=>{
+    container.classList.toggle('active');
+})
